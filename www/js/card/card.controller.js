3ac8eb6d1c037f1f9a823c63e1de627d13903e15
torbjorndahlen <torbjorn.dahlen@icloud.com>
2017-06-15 13:26:25 +0200

    swipe down

--- www/js/card/card.controller.js
+++ www/js/card/card.controller.js
@@ -10,44 +10,74 @@
     $scope.processInstances = [];
     $scope.tasks = [];
 
-    cardService.getTasks().then(
-        function successCallback(response) {
+    function handleTasks() {
 
-          if(response.status != null && response.status != 'SUCCESS') {
-            // Handle FHCloud not calling errorCallback
-            console.log('fail: ' + JSON.stringify(response));
-            //$state.go('error', {error: {"message": JSON.stringify(response), "nextState":'viewDash'}});
-          } else {
-            //console.log('response: ' + JSON.stringify(response));
+      for(var i = 0; i < $scope.processInstances.length; i++) {
+        for(var j = 0; j < $scope.processInstances[i].taskSummaries.length; j++) {
 
-            $scope.processInstances = response.taskInfoList;
-
-            for(var i = 0; i < $scope.processInstances.length; i++) {
-              for(var j = 0; j < $scope.processInstances[i].taskSummaries.length; j++) {
-
-                if($scope.processInstances[i].taskSummaries[j]["activation-time"] != null) {
-                  $scope.processInstances[i].taskSummaries[j]["activation-time"] = new Date($scope.processInstances[i].taskSummaries[j]["activation-time"]).toLocaleString();
-                }
-
-                if($scope.processInstances[i].taskSummaries[j]["expiration-time"] != null) {
-                  $scope.processInstances[i].taskSummaries[j]["expiration-time"] = new Date($scope.processInstances[i].taskSummaries[j]["expiration-time"]).toLocaleString();
-                }
-
-                if($scope.processInstances[i].taskSummaries[j].priority == 0) {
-                  $scope.processInstances[i].taskSummaries[j].priority = "High";
-                } else if($scope.processInstances[i].taskSummaries[j].priority > 0 && $scope.processInstances[i].taskSummaries[j].priority <= 5) {
-                  $scope.processInstances[i].taskSummaries[j].priority = "Medium";
-                } else {
-                  $scope.processInstances[i].taskSummaries[j].priority = "Low";
-                }
-
-                $scope.tasks.push($scope.processInstances[i].taskSummaries[j]);
-              }
-            }
-
+          if($scope.processInstances[i].taskSummaries[j]["activation-time"] != null) {
+            $scope.processInstances[i].taskSummaries[j]["activation-time"] = new Date($scope.processInstances[i].taskSummaries[j]["activation-time"]).toLocaleString();
           }
+
+          if($scope.processInstances[i].taskSummaries[j]["expiration-time"] != null) {
+            $scope.processInstances[i].taskSummaries[j]["expiration-time"] = new Date($scope.processInstances[i].taskSummaries[j]["expiration-time"]).toLocaleString();
+          }
+
+          if($scope.processInstances[i].taskSummaries[j].priority == 0) {
+            $scope.processInstances[i].taskSummaries[j].priority = "High";
+          } else if($scope.processInstances[i].taskSummaries[j].priority > 0 && $scope.processInstances[i].taskSummaries[j].priority <= 5) {
+            $scope.processInstances[i].taskSummaries[j].priority = "Medium";
+          } else {
+            $scope.processInstances[i].taskSummaries[j].priority = "Low";
+          }
+
+          $scope.tasks.push($scope.processInstances[i].taskSummaries[j]);
         }
-      );
+      }
+
+    }
+
+
+    cardService.getTasks().then(
+      function successCallback(response) {
+
+        if(response.status != null && response.status != 'SUCCESS') {
+          // Handle FHCloud not calling errorCallback
+          console.log('fail: ' + JSON.stringify(response));
+          //$state.go('error', {error: {"message": JSON.stringify(response), "nextState":'viewDash'}});
+        } else {
+          //console.log('response: ' + JSON.stringify(response));
+
+          $scope.processInstances = response.taskInfoList;
+
+          handleTasks();
+/*
+          for(var i = 0; i < $scope.processInstances.length; i++) {
+            for(var j = 0; j < $scope.processInstances[i].taskSummaries.length; j++) {
+
+              if($scope.processInstances[i].taskSummaries[j]["activation-time"] != null) {
+                $scope.processInstances[i].taskSummaries[j]["activation-time"] = new Date($scope.processInstances[i].taskSummaries[j]["activation-time"]).toLocaleString();
+              }
+
+              if($scope.processInstances[i].taskSummaries[j]["expiration-time"] != null) {
+                $scope.processInstances[i].taskSummaries[j]["expiration-time"] = new Date($scope.processInstances[i].taskSummaries[j]["expiration-time"]).toLocaleString();
+              }
+
+              if($scope.processInstances[i].taskSummaries[j].priority == 0) {
+                $scope.processInstances[i].taskSummaries[j].priority = "High";
+              } else if($scope.processInstances[i].taskSummaries[j].priority > 0 && $scope.processInstances[i].taskSummaries[j].priority <= 5) {
+                $scope.processInstances[i].taskSummaries[j].priority = "Medium";
+              } else {
+                $scope.processInstances[i].taskSummaries[j].priority = "Low";
+              }
+
+              $scope.tasks.push($scope.processInstances[i].taskSummaries[j]);
+            }
+          }
+*/
+        }
+      }
+    );
 
   $scope.viewVariables = function(event, task) {
     for(var i = 0; i < $scope.processInstances.length; i++) {
@@ -72,6 +102,30 @@
           );
         }
 
+  $scope.onSwipeDown = function(e) {
+    console.log("Swipe down");
+
+    cardService.getTasks().then(
+      function successCallback(response) {
+
+        if(response.status != null && response.status != 'SUCCESS') {
+          // Handle FHCloud not calling errorCallback
+          console.log('fail: ' + JSON.stringify(response));
+          //$state.go('error', {error: {"message": JSON.stringify(response), "nextState":'viewDash'}});
+        } else {
+          //console.log('response: ' + JSON.stringify(response));
+
+          $scope.processInstances = [];
+          $scope.tasks = [];
+
+          $scope.processInstances = response.taskInfoList;
+
+          handleTasks();
+          $scope.$apply();
+        }
+      }
+    );
+  }
 
 
   }]);
--- www/js/push/push.controller.js
+++ www/js/push/push.controller.js
@@ -6,10 +6,8 @@
   .controller('pushController', ['$mdToast', '$mdBottomSheet', '$mdSidenav', '$timeout', '$mdDialog','$rootScope', '$scope', '$state', '$sessionStorage', 'pushService',
   function ($mdToast, $mdBottomSheet, $mdSidenav, $timeout, $mdDialog, $rootScope, $scope, $state, $sessionStorage, pushService){
 
-
       // register with the server to start receiving push notifications
       $fh.push(function(e) {
-
 
       if (e.coldstart) {
         // notification started the app
@@ -18,17 +16,18 @@
       // show text content of the message
       alert(e.alert);
 
+
       // only on iOS
       if (e.badge) {
         push.setApplicationIconBadgeNumber(successHandler, e.badge);
-    }
-  }, function() {
-    console.log("registered for push");
-    $sessionStorage.push = true;
-  }, function(err) {
-    // handle errors
-    console.log("register for push failed: " + err);
-  });
+      }
+
+    }, function() {
+      console.log("registered for push");
+    }, function(err) {
+      // handle errors
+      console.log("register for push failed: " + err);
+    });
 
 
   }]);
